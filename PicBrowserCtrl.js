976daa1f14569f241d12442fcbfdcc6e2b1e9f09
groovymarty <msauser2@gmail.com>
2017-11-19 14:31:55 -0500

    If picture ID specified, scroll to it

--- PicBrowserCtrl.js
+++ PicBrowserCtrl.js
@@ -1,6 +1,6 @@
 angular.module('gvyweb').controller('PicBrowserCtrl', [
-  '$scope', '$stateParams', '$state', 'gvypics',
-  function($scope, $stateParams, $state, gvypics) {
+  '$scope', '$stateParams', '$state', '$timeout', 'gvypics',
+  function($scope, $stateParams, $state, $timeout, gvypics) {
     var placeholder = {
       isPlaceholder: true,
       id: "",
@@ -11,8 +11,12 @@ angular.module('gvyweb').controller('PicBrowserCtrl', [
     $scope.root = placeholder;
     $scope.firstlevel = placeholder;
     $scope.path = [];
-    $scope.cur = placeholder;
+    $scope.curFold = placeholder;
+    var curPicId = null;
     var moreBump;
+    var fixScrollTimer = null;
+    
+    console.log("hello from PicBrowerCtrl");
 
     $scope.rotateTileSz = function(newSz) {
       $scope.sz = newSz || (($scope.sz === "sm") ? "md" : "sm");
@@ -48,41 +52,20 @@ angular.module('gvyweb').controller('PicBrowserCtrl', [
       $scope.showVideo = !$scope.showVideo;
     };
 
-    // pick target for "next" button
-    function pickNextSibling(cur) {
-      if (cur.parentFolder) {
-        var i = cur.parentFolder.folders.indexOf(cur.id) + 1;
-        if (i < cur.parentFolder.folders.length) {
-          $scope.nextId = cur.parentFolder.folders[i];
-        } else {
-          pickNextSibling(cur.parentFolder);
-        }
-      }
-    }
-    function pickNext(cur) {
-      if (cur.folders.length) {
-        // Pick first child, but not if we're at root
-        if (cur.id !== "") {
-          $scope.nextId = $scope.cur.folders[0];
-        }
-      } else {
-        pickNextSibling(cur);
-      }
-    }
-
     // get specified folder and insert at beginning of path
     // continue recursively until root folder reached
     function buildPath(id) {
       return gvypics.getFolder(id).then(function(folder) {
-        if ($scope.cur.isPlaceholder) {
-          $scope.cur = folder;
+        if ($scope.curFold.isPlaceholder) {
+          $scope.curFold = folder;
         }
         if (folder.id === "") {
           if ($scope.firstlevel) {
             $scope.firstlevel.parentFolder = folder;
           }
           $scope.root = folder;
-          pickNext($scope.cur);
+          pickNextFolder($scope.curFold);
+          setCurPic($stateParams.id);
           return true; //done
         } else {
           if ($scope.path[0]) {
@@ -100,5 +83,71 @@ angular.module('gvyweb').controller('PicBrowserCtrl', [
       });
     }
     buildPath($stateParams.id);
+
+    // pick target for "next" button
+    function pickNextFolder(fold) {
+      if (fold.folders.length) {
+        // Pick first child, but not if we're at root
+        if (fold.id !== "") {
+          $scope.nextId = fold.folders[0];
+        }
+      } else {
+        pickNextSibling(fold);
+      }
+    }
+    function pickNextSibling(fold) {
+      if (fold.parentFolder) {
+        var i = fold.parentFolder.folders.indexOf(fold.id) + 1;
+        if (i < fold.parentFolder.folders.length) {
+          $scope.nextId = fold.parentFolder.folders[i];
+        } else {
+          pickNextSibling(fold.parentFolder);
+        }
+      }
+    }
+    
+    // pick current picture
+    function setCurPic(id) {
+      var i = $scope.curFold.pictures.indexOf(id);
+      if (i >= 0) {
+        curPicId = id;
+        console.log("curPicId is "+curPicId);
+        if (i >= $scope.nlimit) {
+          var moreNeeded = i - $scope.nlimit + 1;
+          $scope.nlimit += Math.ceil(moreNeeded / moreBump) * moreBump;
+          console.log("bumping nlimit to "+$scope.nlimit);
+        }
+        $scope.$applyAsync(function() {
+          fixScroll(10);
+        });
+      }
+    }
+    
+    function fixScroll(retries) {
+      if (curPicId) {
+        console.log("searching for "+curPicId);
+        elem = document.getElementById(curPicId);
+        if (elem) {
+          console.log("found");
+          elem.scrollIntoView();
+          cancelFixScrollTimer();
+        } else if (retries && !fixScrollTimer) {
+          fixScrollTimer = $timeout(function() {
+            fixScroll(retries-1);
+          }, 200);
+        }
+      }
+    }
+    
+    function cancelFixScrollTimer() {
+      if (fixScrollTimer) {
+        $timeout.cancel(fixScrollTimer);
+        fixScrollTimer = null;
+      }
+    }
+    
+    $scope.$on('$destroy', function() {
+      cancelFixScrollTimer();
+    });
   }
 ]);
