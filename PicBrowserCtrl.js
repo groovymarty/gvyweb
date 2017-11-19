4bea73f260c550e989eeb9315b5e358555201d72
Marty Sauser <msauser2@gmail.com>
2017-11-19 18:50:24 -0500

    Add current picture and keep it in view

--- PicBrowserCtrl.js
+++ PicBrowserCtrl.js
@@ -12,12 +12,14 @@ angular.module('gvyweb').controller('PicBrowserCtrl', [
     $scope.firstlevel = placeholder;
     $scope.path = [];
     $scope.curFold = placeholder;
-    var curPicId = null;
+    $scope.nlimit = 0;
+    var curPic = null;
     var moreBump;
-    var fixScrollTimer = null;
+    var scrollTimer = null;
+    var lastScrollY = 0;
+    var userScroll = false;
+    var inViewCount = 0;
     
-    console.log("hello from PicBrowerCtrl");
-
     $scope.rotateTileSz = function(newSz) {
       $scope.sz = newSz || (($scope.sz === "sm") ? "md" : "sm");
       switch ($scope.sz) {
@@ -30,17 +32,24 @@ angular.module('gvyweb').controller('PicBrowserCtrl', [
           $scope.tileSzBtnText = "Small Tiles";
           break;
       }
-      $scope.nlimit = moreBump;
+      if ($scope.nlimit < moreBump) {
+        $scope.nlimit = moreBump;
+      }
       $stateParams.sz = $scope.sz;
+      if (curPic) {
+        $scope.$applyAsync(fixScrollReset);
+      }
     };
     $scope.rotateTileSz($stateParams.sz);
     
     $scope.clickPic = function(id) {
+      setCurPic(id);
       if ($scope.sz !== "md") {
         $scope.rotateTileSz();
       } else {
         $state.go('picviewer', {id: id});
       }
+      fixScrollReset();
     };
 
     $scope.morePics = function() {
@@ -58,6 +67,7 @@ angular.module('gvyweb').controller('PicBrowserCtrl', [
       return gvypics.getFolder(id).then(function(folder) {
         if ($scope.curFold.isPlaceholder) {
           $scope.curFold = folder;
+          initCurPic();
         }
         if (folder.id === "") {
           if ($scope.firstlevel) {
@@ -65,7 +75,6 @@ angular.module('gvyweb').controller('PicBrowserCtrl', [
           }
           $scope.root = folder;
           pickNextFolder($scope.curFold);
-          setCurPic($stateParams.id);
           return true; //done
         } else {
           if ($scope.path[0]) {
@@ -106,48 +115,99 @@ angular.module('gvyweb').controller('PicBrowserCtrl', [
       }
     }
     
-    // pick current picture
-    function setCurPic(id) {
+    function initCurPic() {
+      var id = $stateParams.id;
       var i = $scope.curFold.pictures.indexOf(id);
       if (i >= 0) {
-        curPicId = id;
-        console.log("curPicId is "+curPicId);
         if (i >= $scope.nlimit) {
           var moreNeeded = i - $scope.nlimit + 1;
-          $scope.nlimit += Math.ceil(moreNeeded / moreBump) * moreBump;
-          console.log("bumping nlimit to "+$scope.nlimit);
-        }
-        $scope.$applyAsync(function() {
-          fixScroll(10);
-        });
-      }
-    }
-    
-    function fixScroll(retries) {
-      if (curPicId) {
-        console.log("searching for "+curPicId);
-        elem = document.getElementById(curPicId);
-        if (elem) {
-          console.log("found");
-          elem.scrollIntoView();
-          cancelFixScrollTimer();
-        } else if (retries && !fixScrollTimer) {
-          fixScrollTimer = $timeout(function() {
-            fixScroll(retries-1);
-          }, 200);
+          $scope.nlimit += Math.ceil(moreNeeded / 20) * 20;
+          //console.log("bumping nlimit to "+$scope.nlimit);
+          $scope.$applyAsync(initCurPic);
+        } else {
+          setCurPic(id);
+          if (curPic) {
+            fixScrollReset();
+          } else {
+            startScrollTimer(initCurPic, 200);
+          }
         }
       }
     }
     
-    function cancelFixScrollTimer() {
-      if (fixScrollTimer) {
-        $timeout.cancel(fixScrollTimer);
-        fixScrollTimer = null;
+    function setCurPic(id) {
+      if (curPic) {
+        angular.element(curPic).removeClass('current');
+      }
+      if (id) {
+        curPic = document.getElementById(id);
+        if (curPic) {
+          angular.element(curPic).addClass('current');
+        }
+      }
+    }
+    
+    function isInView(elem) {
+      var top = elem.getBoundingClientRect().top;
+      var bottom = elem.getBoundingClientRect().bottom;
+      var wHeight = window.innerHeight;
+      // true if top of element is in view, and bottom also in view if possible
+      return top >= 0 && top < wHeight && (bottom < wHeight || top < 1);
+    }
+    
+    function fixScrollReset() {
+      if (curPic) {
+        inViewCount = 0;
+        userScroll = false;
+        fixScroll();
+      }
+    }
+    
+    function fixScroll() {
+      if (curPic) {
+        if (!isInView(curPic)) {
+          //console.log("not in view");
+          inViewCount = 0;
+          userScroll = false;
+          curPic.scrollIntoView();
+        } else {
+          inViewCount += 1;
+          //console.log("in view "+inViewCount);
+          if (inViewCount >= 2) {
+            userScroll = true;
+          }
+        }
+        startScrollTimer(fixScroll, 500);
+      }
+    }
+    
+    function startScrollTimer(f, t) {
+      cancelScrollTimer();
+      scrollTimer = $timeout(f, t);
+    }
+    
+    function cancelScrollTimer() {
+      if (scrollTimer) {
+        $timeout.cancel(scrollTimer);
+        scrollTimer = null;
       }
     }
     
     $scope.$on('$destroy', function() {
-      cancelFixScrollTimer();
+      cancelScrollTimer();
+    });
+    
+    angular.element(window).on('scroll', function() {
+      var scrollY = window.scrollY;
+      var delta = scrollY - lastScrollY;
+      if (userScroll) {
+        if (delta < -10 || delta > 10) {
+          cancelScrollTimer();
+          lastScrollY = scrollY;
+        }
+      } else {
+        lastScrollY = scrollY;
+      }
     });
   }
 ]);
