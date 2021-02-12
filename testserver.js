761d507a17dd5fb41821e7ccd06113aeb6838094
Marty Sauser <msauser2@gmail.com>
2021-02-12 07:08:24 -0500

    Improve test server

--- testserver.js
+++ testserver.js
@@ -1,8 +1,20 @@
 #!/usr/bin/env nodejs
 var express = require('express');
+var axios = require('axios');
 
 var app = express();
 app.use(express.static("."));
+app.get('/gvypics/pic/*', function(req, res) {
+  res.redirect("https://groovymarty.com"+req.path);
+});
+app.get('/gvypics/vid/*', function(req, res) {
+  res.redirect("https://groovymarty.com"+req.path);
+});
+app.get('/gvypics/ls/*', function(req, res, next) {
+  axios.get("https://groovymarty.com"+req.path)
+    .then(resp => res.json(resp.data))
+    .catch(err => next(err));
+});
 
 var portNum = 3000;
 app.listen(portNum, function() {
